f7f12df9623d43495d5cb9e6b4939bf5e98d8564
代强 <daiqianghaha@foxmail.com>
2018-06-05 22:03:38 +0800

    修改为前三项

--- 7.3.c
+++ 7.3.c
@@ -4,9 +4,10 @@ int main ()
 	int data[21];
 	data[1]=1;
 	data[2]=1;
+	data[3]=2; 
 	int i;
-	for(i=3;i<=20;i++)
-		data[i]=data[i-1]+data[i-2];
+	for(i=4;i<=20;i++)
+		data[i]=data[i-1]+data[i-2]+data[i-3];
 	for(i=1;i<=20;i++)
 		printf("%d ",data[i]);
 	return 0;
