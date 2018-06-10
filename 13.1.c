1e56ef0ee6cb073c02fa2b38cd5e99ef59a530ee
代强 <daiqianghaha@foxmail.com>
2018-06-10 21:26:46 +0800

    fix num bug

--- 13.1.c
+++ 13.1.c
@@ -2,18 +2,18 @@
 
 struct student
 {
-	long int num;
+	long long num;
 	char name[10];
 	int age;
 	double exam[9];
 };
 int main()
 {
-	struct student a[2];
-	int i,flag=1;
-	while(flag--)
+	struct student a[3];
+	int i,flag=2;
+	while(flag>=0)
 	{
-		scanf("%ld",&a[flag].num);
+		scanf("%lld",&a[flag].num);
 		scanf("%s",a[flag].name);
 		scanf("%d",&a[flag].age);
 		for(i=0;i<7;i++)
@@ -22,16 +22,17 @@ int main()
 		for(i=0;i<7;i++)
 			a[flag].exam[7]+=a[flag].exam[i];
 		a[flag].exam[8]=a[flag].exam[7]/7.0;
-		
+		flag--;
 	}
-	flag=1;
-	while(flag--)
+	flag=2;
+	while(flag>=0)
 	{
-		printf("%ld %s %d \n",a[flag].num,a[flag].name,a[flag].age);
+		printf("%lld %s %d \n",a[flag].num,a[flag].name,a[flag].age);
 		for(i=0;i<7;i++)
 			printf("%lf ",a[flag].exam[i]);
 		printf("%lf %lf",a[flag].exam[7],a[flag].exam[8]);
 		printf("\n\n");
+		flag--;
 	}
 	
 }
