2f0922aa6012abfebaf1a5184fa2c6fd9f88f0c8
代强 <daiqianghaha@foxmail.com>
2018-06-12 19:56:37 +0800

    add files
    add files

--- 9.3.c
+++ 9.3.c
@@ -1,35 +1,28 @@
 #include<stdio.h>
-#include<string.h>
-int data[101];
-void cal()
+#include<math.h>
+int judge(int a)
 {
-	int i,j;
-	for(i=2;i<=100;i++)
-		if(data[i])
-			for(j=2;;j++)
-			{
-				if(i*j>100)
-					break;
-				data[i*j]=0;
-			}
-	return;
+	int i;
+	if(a==2)
+		return 1;
+	for(i=2;i<=sqrt(a);i++)
+		if(a%i==0)
+			return 0;
+	return 1;
 }
 int main()
 {
-	int i;
-	memset(data,1,sizeof(int)*101);
-	data[0]=0;
-	data[1]=0;
-	cal();
-	int max=0;
-	int flag=3;
-	while(flag-->0)
+	int a,i,count=0;
+	scanf("%d",&a);
+	printf("%d\n",judge(a));
+	for(i=100;i>=2;i--)
 	{
-		for(i=1;i<=100;i++)
-			if(data[i]&&i>max)
-				max=i;
-	printf("%d ",max);
-	data[max]=0;
-	}	
-	return 0;
+		if(judge(i))
+		{
+			printf("%d ",i);
+			count++;
+		}
+		if(count==3)
+			return 0;
+	}		
 }
