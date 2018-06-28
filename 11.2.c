ebfd7ce7357b68c7f9c50df55226efd226875bae
代强 <daiqianghaha@foxmail.com>
2018-06-28 14:58:20 +0800

    add files
    add files

--- 11.2.c
+++ 11.2.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 int main()
 {
-	char data[12][10];
-	char (*a)[12];
-	int i,j;
-	a=data;
-	*(a[0])="January\0";
-	printf("%s",*(a[0]));
+    char *data[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    int n;
+    printf("请输入月份：");
+    scanf("%d", &n);
+    if (n<1||n>12)
+    	printf("月份输入错误！\n");
+    else
+  	  printf("%s", *(data+n-1));
 	return 0;
-		
-	
- } 
+}
